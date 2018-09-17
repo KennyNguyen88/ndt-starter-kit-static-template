468b914ad10881e4e91c1fa416e0dd2d872b63f3
KennyNguyen88 <giatrangrua@gmail.com>
2018-09-17 21:19:05 +0700

    Intergrate Materialize.css 1.0.0-rc.2

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,12 @@
 import '../scss/app.scss';
-
+import M from 'materialize-css/dist/js/materialize.min';
 //import all images
 var req = require.context("../images", true, /\.(png|svg|jpg|gif|jpeg|ico)$/);
 req.keys().forEach(function(key){
     req(key);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(elems);
 });
