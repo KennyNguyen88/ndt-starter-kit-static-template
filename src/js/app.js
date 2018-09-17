24471c7081810a154dbb19a8cf88fabab9ac4347
AzureAD\NguyenDucTrung <trung.nguyen1@hubcba.com>
2018-09-17 18:05:27 +0700

    Intergrate Bootstrap 4.1.3

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,13 @@
 import '../scss/app.scss';
+import $ from 'jquery';
+import 'bootstrap';
 
 //import all images
 var req = require.context("../images", true, /\.(png|svg|jpg|gif|jpeg|ico)$/);
 req.keys().forEach(function(key){
     req(key);
+});
+
+$(function () {
+    $('[data-toggle="popover"]').popover()
 });
